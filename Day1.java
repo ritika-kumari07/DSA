2d5ec0a0169b099898a05fddfc49e9f10171a644
Ritika Kumari <ritikasingh09@gmail.com>
2026-08-02 16:40:26 +0530

    Day 1 - Multiplication Table

---
+++ Day1.java
@@ -0,0 +1,11 @@
+public class Day1 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Table of " + n);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
