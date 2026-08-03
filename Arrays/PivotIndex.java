5f9c58556fd88c4bb947faf53fa3e2d580f755d4
Ritika Kumari <ritikasingh09@gmail.com>
2026-08-03 16:00:32 +0530

    Solved LeetCode 724 - Find Pivot Index

---
+++ Arrays/PivotIndex.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+class Solution {
+    public int PivotIndex(int[] nums) {
+
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (left == total - left - nums[i]) {
+                return i;
+            }
+
+            left += nums[i];
+        }
+
+        return -1;
+    }
+}
